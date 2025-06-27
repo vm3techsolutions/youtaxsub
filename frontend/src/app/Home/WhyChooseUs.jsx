d786c5747b3ded9880b725dc83eba2d7ab6fed03
Vishal Bhange <vishalbhange333@gmail.com>
2025-06-27 09:42:24 +0530

    Responsive

--- frontend/src/app/Home/WhyChooseUs.jsx
+++ frontend/src/app/Home/WhyChooseUs.jsx
@@ -3,52 +3,54 @@ import React from "react";
 
 export default function WhyChooseUs() {
   return (
-    <div className="mx-auto px-5 sm:px-20 py-16  text-center">
+    <div className="mx-auto px-4 sm:px-10 lg:px-20 py-16 text-center">
       {/* Heading */}
       <div>
-        <h1 className="bg-[#E51D25] text-white px-4 py-1 rounded-lg text-xl font-semibold mb-12 inline-block ml-6">
+        <h1 className="bg-[#E51D25] text-white px-4 py-1 rounded-lg text-xl font-semibold mb-12 inline-block">
           Why Choose Us?
         </h1>
       </div>
 
       {/* Step Boxes */}
-      <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 text-center">
+      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {/* Step 01 */}
         <div className="flex flex-col items-center">
-          <span className="bg-white border-2 border-black text-[#e51d25] w-24 h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-2">
+          <span className="bg-white border-2 border-black text-[#e51d25] w-20 h-20 sm:w-24 sm:h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-4">
             01
           </span>
-          <h3 className="text-black px-14 text-lg font-semibold">Proven 
-          Expertise</h3>
+          <h3 className="text-black text-base sm:text-lg font-semibold max-w-[150px]">
+            Proven Expertise
+          </h3>
         </div>
 
         {/* Step 02 */}
         <div className="flex flex-col items-center">
-          <span className="bg-white border-2 border-black text-[#e51d25] w-24 h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-2">
+          <span className="bg-white border-2 border-black text-[#e51d25] w-20 h-20 sm:w-24 sm:h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-4">
             02
           </span>
-          <h3 className="text-black px-14 text-lg font-semibold">
-          Personalized 
-          Strategies
+          <h3 className="text-black text-base sm:text-lg font-semibold max-w-[150px]">
+            Personalized Strategies
           </h3>
         </div>
 
         {/* Step 03 */}
         <div className="flex flex-col items-center">
-          <span className="bg-white border-2 border-black text-[#e51d25] w-24 h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-2">
+          <span className="bg-white border-2 border-black text-[#e51d25] w-20 h-20 sm:w-24 sm:h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-4">
             03
           </span>
-          <h3 className="text-black px-14 text-lg font-semibold">Proactive 
-          Planning</h3>
+          <h3 className="text-black text-base sm:text-lg font-semibold max-w-[150px]">
+            Proactive Planning
+          </h3>
         </div>
 
         {/* Step 04 */}
         <div className="flex flex-col items-center">
-          <span className="bg-white border-2 border-black text-[#e51d25] w-24 h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-2">
+          <span className="bg-white border-2 border-black text-[#e51d25] w-20 h-20 sm:w-24 sm:h-24 flex items-center justify-center rounded-full text-lg font-semibold sm:text-xl mb-4">
             04
           </span>
-          <h3 className="text-black px-14 text-lg font-semibold">Technology + 
-          Human Touch   </h3>
+          <h3 className="text-black text-base sm:text-lg font-semibold max-w-[150px]">
+            Technology + Human Touch
+          </h3>
         </div>
       </div>
     </div>
