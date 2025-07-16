21c2a0c2cd822368ca853d041cefec51e7d412a9
poojaraut2618 <poojaraut2618@gmail.com>
2025-07-16 13:30:32 +0530

    update title description & seo code

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,8 +6,18 @@ import Testimonial from "./Home/Testimonial";
 import WhyChooseUs from "./Home/WhyChooseUs";
 import WhyUs from "./Home/WhyUs";
 
+export const metadata = {
+  title: "Trusted Tax & Accounting Experts in Pune",
+  description: "Professional CA services in Pune—GST, ITR, TDS, bookkeeping. Trusted by SMEs. Book a consultation today!",
+  other: {
+    "google-site-verification": "6bFyyRDkmHW03sEwvV5QHCco-xMNhhcyxkJ1oIegQ8Y", 
+  },
+};
+
 export default function Home(){
   return(
+    <>
+
     <div>
       <HomeSimplify/>
       <HomeWhatWeDo/>
@@ -17,5 +27,7 @@ export default function Home(){
       <Testimonial/>
       <Plan/>
     </div>
+    </>
+    
   )
 }
