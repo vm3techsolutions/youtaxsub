c046e46fa8a3085334d469f646a86ae0dbf34571
poojaraut2618 <poojaraut2618@gmail.com>
2025-07-16 13:39:04 +0530

    generate sitemap

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://youbuz.in/', 
+  generateRobotsTxt: true, // generates robots.txt file
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
