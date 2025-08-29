2283a6ac8ed972956f04cecfa047e36841846d71
poojaraut2618 <poojaraut2618@gmail.com>
2025-08-29 15:13:33 +0530

    update package.json, next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    reactStrictMode: true,
     images: {
     unoptimized: true, // 👈 disables image optimization
   },
