42c580d757404e096953ab09e30388dc12637121
poojaraut2618 <poojaraut2618@gmail.com>
2025-08-29 11:09:31 +0530

    update changes to deploy site on amplify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["http://localhost:3000/"], // add external domains where images are hosted
+  },
+};
 
-export default nextConfig;
+export default nextConfig;
