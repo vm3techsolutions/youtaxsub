4fe61d49adb78b121d4e0f50a15b08529071e104
poojaraut2618 <poojaraut2618@gmail.com>
2025-08-29 11:22:57 +0530

    update amplify link in next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["http://localhost:3000/"], // add external domains where images are hosted
+    domains: ["http://localhost:3000/", "https://main.d2dpneqojmieou.amplifyapp.com/"], // add external domains where images are hosted
   },
 };
 
