615597b22bd1b9275e13c412d661491ec97f8f3f
poojaraut2618 <poojaraut2618@gmail.com>
2025-08-29 13:15:13 +0530

    chnages for amplify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["http://localhost:3000/", "https://main.d2dpneqojmieou.amplifyapp.com/"], // add external domains where images are hosted
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["http://localhost:3000/", "https://main.d2dpneqojmieou.amplifyapp.com/"], // add external domains where images are hosted
-  },
+  output: "standalone",   // Required for Amplify SSR
 };
 
 export default nextConfig;
